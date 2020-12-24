80bca2e375fa413461d2c5eb71736f4a1d1a21ad
Robert Hou <houyishuang950215@qq.com>
2020-12-24 10:20:26 +0800

    加入对内存分页的支持

--- pagehelperplus/src/main/java/com/hys/pagehelperplus/util/PageHelperUtils.java
+++ pagehelperplus/src/main/java/com/hys/pagehelperplus/util/PageHelperUtils.java
@@ -3,6 +3,7 @@ package com.hys.pagehelperplus.util;
 import com.github.pagehelper.Page;
 import com.hys.pagehelperplus.entity.Pager;
 import com.hys.pagehelperplus.util.mapstruct.PageHelperMapper;
+import lombok.NonNull;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -73,5 +74,60 @@ public class PageHelperUtils<T> {
     public static void remove() {
         KEY_NAMES.remove();
         IS_RELEGATED.remove();
+    }
+
+    /**
+     * 内存分页
+     *
+     * @param list     缓存
+     * @param pageNum  页码
+     * @param pageSize 每页显示数量
+     * @return 分页数据
+     */
+    public static <T> Pager<T> cachePage(@NonNull List<T> list, int pageNum, int pageSize) {
+        if (pageNum < 1 || pageSize < 1) {
+            throw new IllegalArgumentException("页码或每页显示数量不能小于1！");
+        }
+        if (list.size() == 0) {
+            return new Pager<>();
+        }
+
+        int totalRow = list.size();
+        int totalPage;
+        int start;
+        int end;
+        int power = is2Power(pageSize);
+        if (power != -1) {
+            totalPage = (totalRow + pageSize - 1) >>> power;
+            start = (pageNum - 1) << power;
+            end = Math.min(pageNum << power, totalRow);
+        } else {
+            totalPage = (totalRow + pageSize - 1) / pageSize;
+            start = (pageNum - 1) * pageSize;
+            end = Math.min(pageNum * pageSize, totalRow);
+        }
+        List<T> returnList = list.subList(start, end);
+
+        return PageHelperMapper.INSTANCE.pageTransform(
+                (long) start,
+                (long) end,
+                pageNum,
+                pageSize,
+                totalPage,
+                (long) totalRow,
+                returnList);
+    }
+
+    /**
+     * 判断一个数是否是2的幂
+     *
+     * @param value 待判断的数
+     * @return -1：不是2的幂；其他数：2的几次幂
+     */
+    private static int is2Power(int value) {
+        if ((value & (value - 1)) != 0) {
+            return -1;
+        }
+        return Integer.numberOfTrailingZeros(value);
     }
 }
