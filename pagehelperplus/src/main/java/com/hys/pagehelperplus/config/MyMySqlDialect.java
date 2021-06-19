1d4e22da90b0faf4ad7cf093896f83bf25450202
MonkeyOneCool <houyishuang950215@qq.com>
2021-06-19 20:08:09 +0800

    如果字段中只包含主键的话，则不需要优化（因为要查的字段都在B+树上，不需要回表进行查询）。改用默认的SQL后面追加LIMIT子句的方式

--- pagehelperplus/src/main/java/com/hys/pagehelperplus/config/MyMySqlDialect.java
+++ pagehelperplus/src/main/java/com/hys/pagehelperplus/config/MyMySqlDialect.java
@@ -5,11 +5,15 @@ import com.github.pagehelper.dialect.helper.MySqlDialect;
 import com.hys.pagehelperplus.exception.ParseException;
 import com.hys.pagehelperplus.util.PageHelperUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.cache.CacheKey;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 /**
  * 自定义MySQL分页逻辑
@@ -28,9 +32,9 @@ public class MyMySqlDialect extends MySqlDialect {
     public String getPageSql(String sql, Page page, CacheKey pageKey) {
         Matcher containsJoinMatcher = CONTAINS_JOIN_PATTERN.matcher(sql);
         if (containsJoinMatcher.find() || PageHelperUtils.getIsRelegated()) {
-            //多表分页逻辑没实现，用默认的SQL后面追加limit子句的方式（对于不是JOIN方式来进行表连接的SQL（比如笛卡尔积），执行可能会报错。这个时候需要手动将降级选项置为true）
-            PageHelperUtils.remove();
-            return super.getPageSql(sql, page, pageKey);
+            //多表分页逻辑没实现，用默认的SQL后面追加LIMIT子句的方式（对于不是JOIN方式来进行表连接的SQL（比如笛卡尔积），执行可能会报错。这个时候需要手动将降级选项置为true）
+            log.info("使用了多表联查的查询SQL不会进行优化，转而使用默认的SQL后面追加LIMIT子句的方式");
+            return invokeSuperMethod(sql, page, pageKey);
         }
 
         log.info("\n原始SQL：\n{}", sql);
@@ -54,6 +58,12 @@ public class MyMySqlDialect extends MySqlDialect {
 
             //SELECT后面FROM前面的查找字段
             fields = m.group(1);
+
+            if (isOnlyContainsPrimaryKey(fields)) {
+                //如果字段中只包含主键的话，则不需要优化（因为要查的字段都在B+树上，不需要回表进行查询）。改用默认的SQL后面追加LIMIT子句的方式
+                log.info("要查询的字段中只包含主键，不需要优化，转而使用默认的SQL后面追加LIMIT子句的方式");
+                return invokeSuperMethod(sql, page, pageKey);
+            }
 
             if (fields != null) {
                 //查看SQL中是否含有DISTINCT
@@ -98,6 +108,11 @@ public class MyMySqlDialect extends MySqlDialect {
         return returnSql;
     }
 
+    private String invokeSuperMethod(String sql, Page page, CacheKey pageKey) {
+        PageHelperUtils.remove();
+        return super.getPageSql(sql, page, pageKey);
+    }
+
     /**
      * KeyNames转换成String格式（逗号拼接）
      */
@@ -129,5 +144,62 @@ public class MyMySqlDialect extends MySqlDialect {
             }
         }
         return stringBuilder.toString();
+    }
+
+    /**
+     * 判断字段中是否只包含主键
+     */
+    private boolean isOnlyContainsPrimaryKey(String fields) {
+        if (StringUtils.isBlank(fields)) {
+            return false;
+        }
+
+        List<String> keyNames = PageHelperUtils.getKeyNames();
+        if (keyNames == null || keyNames.isEmpty()) {
+            return false;
+        }
+        //考虑keyNames有可能有重复主键名的情况 e.g.@KeyNamesStrategy(keyNames = {"order_id", "order_id"})
+        List<String> uniqueKeyNames = unique(keyNames);
+        String[] fieldArray = fields.split(",");
+        List<String> uniqueFieldArray = unique(fieldArray);
+        int length = 0;
+        for (String field : uniqueFieldArray) {
+            if (!uniqueKeyNames.contains(field)) {
+                return false;
+            }
+            if (uniqueKeyNames.contains(field)) {
+                length++;
+            }
+        }
+        return length == uniqueKeyNames.size();
+    }
+
+    private String removeBackQuoteIfContains(String field) {
+        if (StringUtils.isBlank(field)) {
+            return StringUtils.EMPTY;
+        }
+
+        field = field.trim();
+        if (field.startsWith("`") && field.endsWith("`")) {
+            return field.substring(1, field.length() - 1);
+        }
+        return field;
+    }
+
+    private List<String> unique(List<String> list) {
+        if (list == null || list.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        return list.stream().map(this::removeBackQuoteIfContains).distinct().collect(Collectors.toList());
+    }
+
+    private List<String> unique(String[] array) {
+        if (array == null || array.length == 0) {
+            return Collections.emptyList();
+        }
+
+        List<String> list = Arrays.stream(array).collect(Collectors.toList());
+        return unique(list);
     }
 }
