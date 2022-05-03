2281e8b02663fc6c452d0ffcf847c9dacbfe83f5
MonkeyOneCool <houyishuang950215@qq.com>
2022-05-03 20:19:15 +0800

    Revert "update"
    This reverts commit 27a91db6523366ce8659a093609642ca34d2028c.

--- pagehelperplus/src/test/java/com/hys/pagehelperplus/PagehelperplusApplicationTests.java
+++ pagehelperplus/src/test/java/com/hys/pagehelperplus/PagehelperplusApplicationTests.java
@@ -1,20 +1,13 @@
 package com.hys.pagehelperplus;
 
-import com.hys.pagehelperplus.demo.entity.UserVO;
-import com.hys.pagehelperplus.demo.service.UserService;
-import com.hys.pagehelperplus.entity.Pager;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class PagehelperplusApplicationTests {
 
-    @Autowired
-    private UserService userService;
+	@Test
+	void contextLoads() {
+	}
 
-    @Test
-    void contextLoads() {
-        Pager<UserVO> list = userService.list(1, 10);
-    }
 }
